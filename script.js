fdee030d3de2f4aacfc5dcdfe9c43c974cb6bbf5
MyintKoKoOo <MyintKoKoOo@users.noreply.github.com>
2020-04-14 21:13:24 +0630

    testing

---
+++ script.js
@@ -0,0 +1,83 @@
+var input = document.getElementById("inputitems");
+var button = document.getElementById("btn_add");
+var ol = document.getElementsByClassName("ol")[0];
+var clear_button = document.getElementById("clear_data");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body_bg = document.getElementById("main");
+var text = document.querySelectorAll(".h1color")[0];
+var text2 = document.querySelectorAll(".h1color")[1];
+var choose = document.querySelector("select");
+
+function changeColor(){
+	text.style.color = choose.value;
+	text2.style.color = choose.value;
+}
+
+color1.addEventListener("input", function(){
+	body_bg.style.background = "linear-gradient(to right, "+color1.value+", "+color2.value+")";
+});
+color2.addEventListener("input", function(){
+	body_bg.style.background = "linear-gradient(to right, "+color1.value+", "+color2.value+")";
+});
+
+function inputlength(){
+	return input.value.length;
+}
+var item = [
+	{
+		item_name : "apple",
+		price : "400Ks"
+	},
+	{
+		code : "clear()"
+	}
+];
+function click_function(){
+	if(inputlength()>0 && input.value !== item[0].item_name && input.value !== item[1].code){
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.setAttribute("class", "li_part");
+	ol.appendChild(li);
+	input.value = "";
+}
+	if(inputlength()>0 && input.value === item[0].item_name && input.value !== item[1].code){
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(item[0].item_name+" : "+item[0].price));
+	li.setAttribute("class", "li_part");
+	ol.appendChild(li);
+	input.value = "";
+	}
+	if(inputlength()>0 && input.value === item[1].code){
+	input.value = "";
+	ol.innerHTML = "";
+	}
+}
+function keypress_function(){
+	if(inputlength()>0 && event.which === 13 && input.value !== item[0].item_name && input.value !== item[1].code){
+	var li = document.createElement("li");
+	li.setAttribute("class", "li_part");
+	li.appendChild(document.createTextNode(input.value));
+	ol.appendChild(li);
+	input.value = "";
+}
+	if(inputlength()>0 && input.value === item[0].item_name && event.which === 13 && input.value !== item[1].code){
+	var li = document.createElement("li");
+	li.setAttribute("class", "li_part");
+	li.appendChild(document.createTextNode(item[0].item_name+" : "+item[0].price));
+	ol.appendChild(li);
+	input.value = "";
+	}
+	if(inputlength()>0 && input.value === item[1].code && event.which === 13){
+	input.value = "";
+	ol.innerHTML = "";
+	}
+}
+
+button.addEventListener("click", click_function);
+input.addEventListener("keypress", keypress_function);
+
+// clear_button.addEventListener("click", function(){
+// 	return ol.innerHTML = "";
+// });
+
