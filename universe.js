be542c2e1e1859561458f6d2c8a284316c8c0ba3
MyintKoKoOo <33740102+MyintKoKoOo@users.noreply.github.com>
2020-05-27 17:28:29 +0630

    Update universe.js

--- universe.js
+++ universe.js
@@ -1,6 +1,15 @@
+const selectname = document.getElementById("selectname");
 const rangebar = document.getElementById("myrange");
 const rangenum = document.getElementById("rangenum");
+const button = document.getElementById("mainbtn");
 function rangefunction(){
 	rangenum.innerHTML=rangebar.value;
 }
 rangebar.addEventListener("input",rangefunction);
+ 
+const decide = () =>{
+	if(selectname.value === "empty"){
+		alert("နာမည်ကိုရွေးပေးပါဦးခင်ဗျ");
+	}
+}
+button.addEventListener("click",decide);
