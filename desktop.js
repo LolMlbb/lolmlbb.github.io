e3ecccbf461d35935d9f4c761b117cab242fd4dd
MyintKoKoOo <33740102+MyintKoKoOo@users.noreply.github.com>
2020-07-12 11:12:46 +0630

    testing

---
+++ desktop.js
@@ -0,0 +1,26 @@
+const lang = navigator.language;
+let datename = document.getElementsByClassName("datename")[0];
+let datenum = document.getElementsByClassName("datenum")[0];
+let monthname = document.getElementsByClassName("monthname")[0];
+let clock = document.getElementsByClassName("clock")[0];
+
+
+const showTime = () =>{
+const today = new Date();
+let h = today.getHours();
+let m = today.getMinutes();
+let s = today.getSeconds();
+m = checkTime(m);
+s = checkTime(s);
+clock.innerHTML = `${h}:${m}:${s}`;
+setTimeout(showTime,500);
+datenum.innerHTML = today.getDate();
+datename.innerHTML = today.toLocaleString(lang,{weekday: 'long'});
+monthname.innerHTML = today.toLocaleString(lang,{month: 'long'});
+}
+const checkTime = (i) => {
+	if(i<10){
+		i = "0"+i;
+	}
+	return i;
+}
